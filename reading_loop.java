54c664e34d34e9ef4f79d09322cdefadb6779f36
Elena Sheina <helena.bohn@yandex.ru>
2022-12-07 17:25:27 +0500

    add reading_loop.java

--- reading_loop.java
+++ reading_loop.java
@@ -8,10 +8,10 @@ public class Praktikum {
   public static void checkReading(int freeHours) {// метод принимает количество свободного времени как параметр
     if (freeHours >= 2) { // Цикл сработает, только если есть два или больше часа свободного времени
         for (int i = 1; i < 4; i = i + 1) {
-          System.out.println("Анатолий прочитал " + i + "-ю главу");
+          System.out.println(Анатолий прочитал  + i + -ю главу);
         }
     } else {
-        System.out.println("Сегодня не читаем.");
+        System.out.println(Сегодня не читаем.);
     }
   }
 
